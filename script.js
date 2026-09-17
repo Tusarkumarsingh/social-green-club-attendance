ade44a67ac41ad6439d9fdb214902d12de3f4079
tusarkumarsigh <ts310491@gmail.com>
2026-09-17 14:10:08 +0530

    Added app installation and logo

--- script.js
+++ script.js
@@ -4247,3 +4247,17 @@ document.addEventListener(
 
     }
 );
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("./service-worker.js")
+            .then(() => {
+                console.log("Service Worker registered successfully");
+            })
+            .catch(error => {
+                console.error("Service Worker registration failed:", error);
+            });
+    });
+}
